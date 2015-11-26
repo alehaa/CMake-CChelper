819854d4a31f610980f96f94968a8cb81ab0fbe6
Alexander Haase <alexander.haase@rwth-aachen.de>
2015-11-26 17:23:35 +0100

    Added new CMake module to lookup compiler flags for C99.

---
+++ cmake/FindC99.cmake
@@ -0,0 +1,47 @@
+# This file is part of CMake-CChelper.
+#
+# Copyright (C)
+#   2015 Alexander Haase <alexander.haase@rwth-aachen.de>
+#
+# See the LICENSE file in the base directory for details.
+# All rights reserved.
+#
+
+set(C99_FLAG_CANDIDATES
+	"-std=gnu99"
+	"-std=c99"
+)
+
+
+include(CheckCCompilerFlag)
+include(FindPackageHandleStandardArgs)
+
+set(CMAKE_REQUIRED_QUIET_SAVE ${CMAKE_REQUIRED_QUIET})
+set(CMAKE_REQUIRED_QUIET ${ASan_FIND_QUIETLY})
+
+
+if (NOT CMAKE_C_COMPILER_LOADED)
+	message(SEND_ERROR "C99 requires C language to be enabled")
+endif ()
+
+# If flags for this compiler were already found, do not try to find them
+# again.
+if (C99_FLAGS)
+	return()
+endif ()
+
+foreach (FLAG ${C99_FLAG_CANDIDATES})
+	if(NOT CMAKE_REQUIRED_QUIET)
+		message(STATUS "Try C99 flag = [${FLAG}]")
+	endif()
+
+	check_c_compiler_flag("${FLAG}" C99_FLAG_DETECTED)
+	if (C99_FLAG_DETECTED)
+		set(C99_FLAGS "${FLAG}" CACHE STRING "C compiler flags for C99")
+		break()
+	endif ()
+endforeach ()
+
+
+find_package_handle_standard_args(C99 REQUIRED_VARS C99_FLAGS)
+mark_as_advanced(C99_FLAGS)
